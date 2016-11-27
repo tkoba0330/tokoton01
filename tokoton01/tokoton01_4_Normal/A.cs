c100a0264d326852bc5feb4e2b7456a9fe21d76a
t.koba0330@gmail.com <t.koba0330@gmail.com>
2016-11-27 11:50:10 +0900

    とことんC# No.1終了

---
+++ tokoton01/tokoton01_4_Normal/A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tokoton01_4_Normal
+{
+    public static class A
+    {
+        public static Action Say = () => Console.WriteLine("My name is A!");
+
+        public static void SayWait()
+        {
+            Say();
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ tokoton01/tokoton01_4_Normal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tokoton01_4_Normal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A.Say = () => Console.WriteLine("My name is B");
+
+            A.Say();
+
+        }
+    }
+}
